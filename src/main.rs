bfd82aabe5e5113388a2b49d7abaac0c5da48ade
Yaman Arat <yamanarat@gmail.com>
2026-06-29 12:57:23 +0300

    feat: automated list building

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused)]
 use itertools::Itertools;
-use std::fs;
+use std::{fs, process};
 use std::env::args;
 use std::time::Instant;
 
@@ -15,10 +15,24 @@ fn is_vowel(a: char, vowels: &str, consonants: &str) -> bool {
     false
 }
 
+fn list_add(paths: &[String]) {
+    
+    let mut list = fs::read_to_string("list.txt").unwrap_or_default();
+    for path in paths {
+        let pathsplit: Vec<&str> = path.split("/").collect();
+        let language = pathsplit[1];
+        let contents = format!("{path}-{language} \n");
+        list.push_str(&contents);
+    }
+    fs::write("list.txt", list).unwrap();
+    process::exit(0);
+}
+
 fn analyze(vowels: &str, consonants: &str, args: Vec<&str>) {
     let a = fs::read_to_string(&args[0]).unwrap();
     let language = &args[1];
-    let comment = &args[2];
+    let fullpath: Vec<&str> = args[0].split("/").collect();
+    let path = fullpath[2];
     let mut cc = 0;
     let mut cv = 0;
     let mut vc = 0;
@@ -52,7 +66,7 @@ fn analyze(vowels: &str, consonants: &str, args: Vec<&str>) {
     let vvp = (vv as f64 / total as f64) * 100.00;
     let ratio = (cons as f64 / vow as f64) * 100.00;
     println!("CC:{:.4}% CV:{:.4}% VC:{:.4}% VV:{:.4}% C/V:{:.4}%", ccp, cvp, vcp, vvp, ratio);
-    let write = format!("{language} CC:{:.4}% CV:{:.4}% VC:{:.4}% VV:{:.4}% C/V:{:.4}% Comments: {comment}\n", ccp, cvp, vcp, vvp, ratio);
+    let write = format!("{language} CC:{:.4}% CV:{:.4}% VC:{:.4}% VV:{:.4}% C/V:{:.4}% Path: {path}\n", ccp, cvp, vcp, vvp, ratio);
     let write = fs::read_to_string("data.txt").unwrap() + &write;
     fs::write("data.txt", write).unwrap();
 }
@@ -68,6 +82,18 @@ fn main() {
     let args: Vec<String> = args().collect();
     if args.len() == 2 && args[1] == "redo" {
         fs::write("data.txt", "").unwrap();
+    }
+    let length = args.len();
+
+    if args.len() > 2 && args[1] == "list" {
+        if args[2] == "clear" {
+            fs::write("list.txt", "").unwrap();
+            process::exit(0);
+        }
+        else if args[2] == "add" {
+            list_add(&args[3..length]);
+            process::exit(0);
+        }
     }
 
     let length = list.len();
