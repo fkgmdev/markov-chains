c49957186470effba02f47fd02d2bfa0c070697e
Yaman Arat <yamanarat@gmail.com>
2026-09-15 20:52:46 +0300

    feat, bugfix: seperated stuff into lib.rs, refixed supposedly fixed bug?

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,21 @@
 #[allow(unused)]
 use itertools::Itertools;
+use markov_chains::{analyze, list_add};
 use std::env::args;
 use std::time::Instant;
 use std::{fs, process};
 
-fn is_vowel(a: char, vowels: &str, consonants: &str) -> bool {
-    let lower = a.to_lowercase().next().unwrap_or(a);
-    if vowels.chars().contains(&lower) {
-        return true;
-    } else if consonants.chars().contains(&lower) {
-        return false;
-    }
-    false
-}
-
-fn list_add(paths: &[String]) {
-    let mut list = fs::read_to_string("list.txt").unwrap_or_default();
-    for path in paths {
-        let pathsplit: Vec<&str> = path.split("/").collect();
-        let language = pathsplit[1];
-        let contents = format!("{path}-{language} \n");
-        list.push_str(&contents);
-    }
-    fs::write("list.txt", list).unwrap();
-    process::exit(0);
-}
-
-fn analyze(vowels: &str, consonants: &str, args: Vec<&str>) {
-    let a = fs::read_to_string(args[0]).unwrap();
-    let language = &args[1];
-    let fullpath: Vec<&str> = args[0].split("/").collect();
-    let path = fullpath[2];
-    let mut vvv = 0;
-    let mut vvc = 0;
-    let mut vcv = 0;
-    let mut vcc = 0;
-    let mut cvv = 0;
-    let mut cvc = 0;
-    let mut ccv = 0;
-    let mut ccc = 0;
-
-    let mut cons = 0;
-    let mut vow = 0;
-    for (a1, a2, a3) in a.chars().tuple_windows() {
-        if a1.is_alphabetic() && a2.is_alphabetic() && a3.is_alphabetic() {
-            match (
-                is_vowel(a1, vowels, consonants),
-                is_vowel(a2, vowels, consonants),
-                is_vowel(a3, vowels, consonants),
-            ) {
-                (true, true, true) => vvv += 1,
-                (true, true, false) => vvc += 1,
-                (true, false, true) => vcv += 1,
-                (true, false, false) => vcc += 1,
-                (false, true, true) => cvv += 1,
-                (false, true, false) => cvc += 1,
-                (false, false, true) => ccv += 1,
-                (false, false, false) => ccc += 1,
-            }
-        }
-    }
-    for char in a.chars() {
-        if char.is_alphabetic() {
-            if is_vowel(char, vowels, consonants) {
-                vow += 1;
-            } else {
-                cons += 1;
-            }
-        }
-    }
-    let total = vvv + vvc + vcv + vcc + cvv + cvc + ccv + vvv;
-    let vvvp = (vvv as f64 / total as f64) * 100.00;
-    let vvcp = (vvc as f64 / total as f64) * 100.00;
-    let vcvp = (vcv as f64 / total as f64) * 100.00;
-    let vccp = (vcc as f64 / total as f64) * 100.00;
-    let cvvp = (cvv as f64 / total as f64) * 100.00;
-    let cvcp = (cvc as f64 / total as f64) * 100.00;
-    let ccvp = (ccv as f64 / total as f64) * 100.00;
-    let cccp = (ccc as f64 / total as f64) * 100.00;
-
-    let ratio = (cons as f64 / vow as f64) * 100.00;
-    println!(
-        "VVV:{:.4}% VVC:{:.4}% VCV:{:.4}% VCC:{:.4}% CVV:{:.4}% CVC:{:.4}% CCV:{:.4}% CCC:{:.4}%",
-        vvvp, vvcp, vcvp, vccp, cvvp, cvcp, ccvp, cccp,
-    );
-    let write = format!(
-        "{language} VVV:{:.4}% VVC:{:.4}% VCV:{:.4}% VCC:{:.4}% CVV:{:.4}% CVC:{:.4}% CCV:{:.4}% CCC:{:.4}% C/V:{:.4}% CPath: {path}\n",
-        vvvp, vvcp, vcvp, vccp, cvvp, cvcp, ccvp, cccp, ratio,
-    );
-    let write = fs::read_to_string("data.txt").unwrap() + &write;
-    fs::write("data.txt", write).unwrap();
+struct Profile {
+    name: String,
+    vvvp: f64,
+    vvcp: f64,
+    vcvp: f64,
+    vccp: f64,
+    cvvp: f64,
+    cvcp: f64,
+    ccvp: f64,
+    cccp: f64,
+    cvrp: f64,
 }
 
 fn main() {
