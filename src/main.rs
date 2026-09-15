32f9102b97ce8e5afb0a0b7123444196a31a7787
Yaman Arat <yamanarat@gmail.com>
2026-09-15 20:13:40 +0300

    bugfix: fixed huge mistake where CCC combos werent counted at all

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn analyze(vowels: &str, consonants: &str, args: Vec<&str>) {
     let mut cvv = 0;
     let mut cvc = 0;
     let mut ccv = 0;
-    let mut vvv = 0;
+    let mut ccc = 0;
 
     let mut cons = 0;
     let mut vow = 0;
@@ -56,7 +56,7 @@ fn analyze(vowels: &str, consonants: &str, args: Vec<&str>) {
                 (false, true, true) => cvv += 1,
                 (false, true, false) => cvc += 1,
                 (false, false, true) => ccv += 1,
-                (false, false, false) => vvv += 1,
+                (false, false, false) => ccc += 1,
             }
         }
     }
@@ -77,16 +77,16 @@ fn analyze(vowels: &str, consonants: &str, args: Vec<&str>) {
     let cvvp = (cvv as f64 / total as f64) * 100.00;
     let cvcp = (cvc as f64 / total as f64) * 100.00;
     let ccvp = (ccv as f64 / total as f64) * 100.00;
-    let vvvp = (vvv as f64 / total as f64) * 100.00;
+    let cccp = (ccc as f64 / total as f64) * 100.00;
 
     let ratio = (cons as f64 / vow as f64) * 100.00;
     println!(
-        "VVV:{:.4}% VVC:{:.4}% VCV:{:.4}% VCC:{:.4}% CVV:{:.4}% CVC:{:.4}% CCV:{:.4}% VVV:{:.4}%",
-        vvvp, vvcp, vcvp, vccp, cvvp, cvcp, ccvp, vvvp,
+        "VVV:{:.4}% VVC:{:.4}% VCV:{:.4}% VCC:{:.4}% CVV:{:.4}% CVC:{:.4}% CCV:{:.4}% CCC:{:.4}%",
+        vvvp, vvcp, vcvp, vccp, cvvp, cvcp, ccvp, cccp,
     );
     let write = format!(
-        "{language} VVV:{:.4}% VVC:{:.4}% VCV:{:.4}% VCC:{:.4}% CVV:{:.4}% CVC:{:.4}% CCV:{:.4}% VVV:{:.4}% C/V:{:.4}% CPath: {path}\n",
-        vvvp, vvcp, vcvp, vccp, cvvp, cvcp, ccvp, vvvp, ratio,
+        "{language} VVV:{:.4}% VVC:{:.4}% VCV:{:.4}% VCC:{:.4}% CVV:{:.4}% CVC:{:.4}% CCV:{:.4}% CCC:{:.4}% C/V:{:.4}% CPath: {path}\n",
+        vvvp, vvcp, vcvp, vccp, cvvp, cvcp, ccvp, cccp, ratio,
     );
     let write = fs::read_to_string("data.txt").unwrap() + &write;
     fs::write("data.txt", write).unwrap();
